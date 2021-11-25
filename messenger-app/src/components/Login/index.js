a5ed2444814a649057002414169ffb52c52a9e96
Gabriel Rodrigues <gabriel.ivi@hotmail.com>
2021-11-25 13:02:33 -0400

    [FEATURE] Criado listagem dos emails (#1)

--- messenger-app/src/components/Login/index.js
+++ messenger-app/src/components/Login/index.js
@@ -1,77 +1,82 @@
-import { Component } from 'react'
+import { useState } from 'react'
 import { Navigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setUsuario } from '../../actions'
 import axios from 'axios'
 import './style.css'
 
-class Login extends Component {
+function Login() {
+    const [informacoesInvalidas, setInformacoesInvalidas] = useState(false)
+    const [loginRealizado, setLoginRealizado] = useState(false)
+    const [mensagemErro, setMensagemErro] = useState('')
+    const [informacoesUsuario, setInformacoesUsuario] = useState({})
+    const [manterConectado, setManterConectado] = useState(false)
+    const dispatch = useDispatch()
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            informacoesInvalidas: false,
-            loginRealizado: false
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    async handleSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault()
-        axios.post('http://localhost:1337/usuarios/login', this.state).then(response => {
-            localStorage.setItem('token', response.data.token)
-            this.setState({informacoesInvalidas: false})
-            this.setState({loginRealizado: true})
-        }).catch(() => {
-            this.setState({informacoesInvalidas: true})
+        axios.post('http://localhost:1337/usuarios/login', informacoesUsuario).then(response => {
+            if (manterConectado) {
+                localStorage.setItem('token', response.data.token)
+            } else {
+                dispatch(setUsuario(response.data))
+            }
+            setInformacoesInvalidas(true)
+            setLoginRealizado(true)
+        }).catch((err) => {
+            setInformacoesInvalidas(true)
+            setMensagemErro(err.response.data.message)
         })
     }
 
-    handleChangeInformacoes({target}, campo) {
-        this.setState({[campo]: target.value})
+    function handleChangeInformacoes({target}, campo) {
+        const dados = Object.assign(informacoesUsuario, {})
+        dados[campo] = target.value
+        setInformacoesUsuario({...dados})
     }
 
-    render() {
-        if(this.state.loginRealizado) {
-            return <Navigate to='/painel'/>;
-        }        
-        return (
-            <div className="vh-100 d-flex justify-content-center align-items-center">
-                <div className="d-flex flex-column align-items-center">
-                    <h2>Messanger APP</h2>
-                    <form onSubmit={this.handleSubmit} className="d-flex flex-column border border-2 rounded p-5">
-                        <div className="w-100 h-100 d-flex justify-content-center">
-                            <h4>Entrar</h4>
-                        </div>
-                        <label htmlFor="email">E-mail</label>
-                        <input
-                            value={this.state.email || ''}
-                            onChange={event => {this.handleChangeInformacoes(event, "email")}} 
-                            className="form-control"
-                            type="email"
-                            id="email"
-                        />
-                        <label htmlFor="senha">Senha</label>
-                        <input
-                            value={this.state.senha || ''}
-                            onChange={event => {this.handleChangeInformacoes(event, "senha")}} 
-                            className="form-control" 
-                            type="password" 
-                            id="senha"
-                        />
-    
-                        <div className="d-flex justify-content-between mt-3 mb-3">
-                            <div className="d-flex align-items-center">
-                                <input id="ficarConectado" className="me-2" type="checkbox"/>
-                                <label htmlFor="ficarConectado">Ficar conectado</label>
-                            </div>
-                            <a href="/cadastro" className="text-decoration-none">Criar uma conta</a>
-                        </div>
-                        <button className="btn btn-primary">Entrar</button>
-                        {this.state.informacoesInvalidas && <p className="informacoesInvalidas mb-1 mt-1">E-mail ou senha inválido.</p>}
-                    </form>
-                </div>
-            </div>
-        )
+    function handleChangeManterConectado({target}) {
+        setManterConectado(target.checked)
     }
+ 
+    return loginRealizado ? (<Navigate to='/painel'/>) : (
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-column align-items-center">
+                <h2>Messanger APP</h2>
+                <form onSubmit={handleSubmit} className="d-flex flex-column border border-2 rounded p-5">
+                    <div className="w-100 h-100 d-flex justify-content-center">
+                        <h4>Entrar</h4>
+                    </div>
+                    <label htmlFor="email">E-mail</label>
+                    <input
+                        value={informacoesUsuario.email || ''}
+                        onChange={event => {handleChangeInformacoes(event, "email")}} 
+                        className="form-control"
+                        type="email"
+                        id="email"
+                    />
+                    <label htmlFor="senha">Senha</label>
+                    <input
+                        value={informacoesUsuario.senha || ''}
+                        onChange={event => {handleChangeInformacoes(event, "senha")}} 
+                        className="form-control" 
+                        type="password" 
+                        id="senha"
+                    />
+
+                    <div className="d-flex justify-content-between mt-3 mb-3">
+                        <div className="d-flex align-items-center">
+                            <input id="ficarConectado" className="me-2" type="checkbox" onChange={handleChangeManterConectado}/>
+                            <label htmlFor="ficarConectado">Ficar conectado</label>
+                        </div>
+                        <a href="/cadastro" className="text-decoration-none">Criar uma conta</a>
+                    </div>
+                    <button className="btn btn-primary">Entrar</button>
+                    {informacoesInvalidas && <p className="text-danger mb-1 mt-1">{mensagemErro}</p>}
+                </form>
+            </div>
+        </div>
+    )
     
 }
 
---
+++ messenger-app/src/components/Menu/index.js
@@ -0,0 +1,38 @@
+import './style.css'
+import { useDispatch , useSelector} from 'react-redux'
+import { setPaginaAtual } from '../../actions'
+
+function Menu() {
+    const { emailsRecebidos, emailsEnviados } = useSelector(state => state)
+
+    const dispatch = useDispatch()
+
+    function handleChangePaginaAtual(event, pagina) {
+        const paginaRenderizada  = pagina || 'entrada'
+        if(event) event.preventDefault()
+        const opcoes = document.querySelectorAll('.opcoes')
+        for(let opcao of opcoes) {
+            opcao.classList.add('text-dark')
+        }
+        document.querySelector(`#${paginaRenderizada}`).classList.remove('text-dark')
+        dispatch(setPaginaAtual(paginaRenderizada))
+    }
+
+    return (
+        <div className="menu d-flex flex-column justify-content-start border-end">
+            <div className="d-flex flex-column align-items-center w-100 mt-2">
+                <button className="btn btn-primary botaoNovo"><i className="fas fa-pencil-alt me-1"></i>Nova mensagem</button>
+            </div>
+            <a id="entrada" onClick={event => handleChangePaginaAtual(event, "entrada")} className="opcoes ps-4 pt-3 text-decoration-none border-bottom d-flex justify-content-between" href="/#">
+                <p  className="text-over-flow me-1 mb-0"><i className="far fa-envelope me-1"></i> Caixa de entrada</p>
+                <strong className="me-4">{emailsRecebidos.length}</strong>
+            </a>
+            <a id="enviados" onClick={event => handleChangePaginaAtual(event, "enviados")} className="opcoes ps-4 pt-3 text-decoration-none border-bottom d-flex justify-content-between text-dark" href="/#">
+                <p className=" text-over-flow me-1 mb-0"><i className="far fa-paper-plane me-2"></i>Itens enviados</p>
+                <strong className="me-4">{emailsEnviados.length}</strong>
+            </a>
+        </div>
+    )
+}
+
+export default Menu
